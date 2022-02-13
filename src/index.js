77bed759db75423a3cfbb68ebc2d65c113d9d821
Berkant Can Erkanat <berkantcanerkanat44@gmail.com>
2022-02-14 00:42:05 +0300

    final

--- src/index.js
+++ src/index.js
@@ -20,24 +20,81 @@ const ui = new Ui();
 //event listeners
 addEventListeners();
 
+//update etmek ıcın genel bir id tanımlıyorum
+let idToBeUpdated = 0;
+
 function addEventListeners() {
-    document.addEventListener("DOMContentLoaded",getAllEmployees);
+    document.addEventListener("DOMContentLoaded",showAllEmployees);
+    form.addEventListener("submit",addNewEmployee);
+    employeesList.addEventListener("click",updateOrdeleteEmployee);
+    updateEmployeeButton.addEventListener("click",updateTheEmployee);
 }
-function getAllEmployees(){
+function updateTheEmployee(){
+    let name = nameInput.value.trim();
+    let dept = deptInput.value.trim();
+    let sal = salInput.value.trim();
+    if(!checkForRightInput(name,dept,sal)){
+        alert("Lutfen tum alanları doldurun");
+     }else {
+        ui.clearInputs(nameInput,deptInput,salInput);
+        ui.deActivateUpdateButton(updateEmployeeButton);
+        request.put(idToBeUpdated,{name:name,departmant:dept,salary:Number(sal)})
+        .then(changedEmployee => showAllEmployees()) //resolvu burda yakaladık
+        .catch(err => console.error(err));
+     }
+}
+function updateOrdeleteEmployee(e){
+    if(e.target.id === "delete-employee"){
+        let child = e.target.parentNode.parentNode;
+        let id = e.target.parentNode.previousElementSibling.previousElementSibling.textContent;
+        if(confirm(`id ${id} çalışanı silmek istediğinize emin misiniz`)){
+            request.delete(id)
+            .then(message => ui.deleteEmployeeFromUI(employeesList,child))
+            .catch(error => console.error(error));
+        }
+    }else if(e.target.id === "update-employee"){
+        //güncelleye basılmıs ıse guncelle butonu aktıf olup input alanları bılgıler ıle dolucak
+        idToBeUpdated = e.target.parentNode.previousElementSibling.textContent;
+        nameInput.value = e.target.parentNode.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.textContent;
+        deptInput.value = e.target.parentNode.previousElementSibling.previousElementSibling.previousElementSibling.textContent;
+        salInput.value = e.target.parentNode.previousElementSibling.previousElementSibling.textContent;
+        
+        ui.activateUpdateButton(updateEmployeeButton);
+    }
+
+}
+function showAllEmployees(){ // api den getirir show eder
     request.get()
     .then(employees => ui.addEmployeesToUI(employeesList,employees))
     .catch(err => console.error(err));
 }
+function addNewEmployee(e){
+    //inputları al bosluk kontrol et rest api yaz ui da guncelle
+    let name = nameInput.value.trim();
+    let dept = deptInput.value.trim();
+    let sal = salInput.value.trim();
+    if(!checkForRightInput(name,dept,sal)){
+       alert("Lutfen tum alanları doldurun");
+    }else {
+        addEmployeeToRestApi(name,dept,Number(sal));
+    }
+    ui.clearInputs(nameInput,deptInput,salInput);
+    e.preventDefault();
+}
+function addEmployeeToRestApi(name,dept,sal){ // hem rest api yukler hem de show eder
+request.post({name:name,departmant:dept,salary:sal})
+.then(addedEmployee => ui.addNewEmployeeToUI(employeesList,addedEmployee))
+.catch(err => console.error(err));
+}
+function checkForRightInput(name,dept,sal){
+    if(name === "" || dept === "" || sal === ""){
+        return false;
+    }else{
+        return true;
+    }
+}
 
-// //POST
-// request.post({name:"Seren Erkanat",departmant:'HR',salary:10000})
-// .then(addedEmployee => console.log(addedEmployee)) //resolvu burda yakaladık
-// .catch(err => console.error(err));
 //PUT id yi url de verdik onun ıcın update yapmadık id si 1 olan postun değerlerini değiştik
 // request.put(2,{name:"deneme",departmant:'deneme',salary:30000})
 // .then(changedEmployee => console.log(changedEmployee)) //resolvu burda yakaladık
 // .catch(err => console.error(err));
-//DELETE
-// request.delete(1)
-// .then(message => console.log(message))
-// .catch(error => console.error(error));
--- src/ui.js
+++ src/ui.js
@@ -12,6 +12,30 @@ export class Ui {
         </tr> `
         });
         employeesList.innerHTML = result;
-
+    }
+    clearInputs(nameInput,deptInput,salInput) {
+        nameInput.value = "";
+        deptInput.value = "";
+        salInput.value = "";
+    }
+    addNewEmployeeToUI(employeesList,employee){
+        let result = `<tr>                                 
+        <td>${employee.name}</td>
+        <td>${employee.departmant}</td>
+        <td>${employee.salary}</td>
+        <td>${employee.id}</td>
+        <td><a href="#" id = "update-employee" class= "btn btn-danger">Güncelle</a></td> 
+        <td> <a href="#" id = "delete-employee" class= "btn btn-danger">Sil</a></td>
+    </tr> `
+    employeesList.innerHTML += result;
+    }
+    deleteEmployeeFromUI(employeesList,child){
+        employeesList.removeChild(child);
+    }
+    activateUpdateButton(updateEmployeeButton){
+        updateEmployeeButton.style.display = "block";
+    }
+    deActivateUpdateButton(updateEmployeeButton){
+        updateEmployeeButton.style.display = "none";
     }
 }
